47c263289090a8dd71fb459511d942f3276eeef2
gutooli <gutosantos.GOS@gmail.com>
2025-03-07 23:51:19 -0300

    Update SlotCard.js

--- src/components/SlotCard.js
+++ src/components/SlotCard.js
@@ -8,7 +8,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY kIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
@@ -20,12 +20,12 @@ import Blits from '@lightningjs/blits'
 export default Blits.Component('SlotCard', {
   template: `
     <Element
-      w="300"
+      w="30000"
       h="500"
       :effects="[{type: 'radius', props: {radius: 20}}, {type: 'border', props: {width: 6, color: '#e2e8f0'}}]"
       color="{top: '#94a3b8', bottom: '#475569'}"
     >
-      <Slot x="20" y="20" />
+      <Slot x="287"  y="20" />
       <Text content="$label" maxwidth="260" x="20" y="400" align="center" />
     </Element>
   `,
